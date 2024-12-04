ecc7d565b3749b62df36d2d8a9536518a7e3479d
Luke Diliberto <lukediliberto@gmail.com>
2024-12-04 13:17:22 -0800

    include budget

--- aws/account/main.tf
+++ aws/account/main.tf
@@ -1,3 +1,8 @@
+locals {
+  # Use subscriber_emails if provided, otherwise fall back to account_email
+  notification_emails = length(var.budget_config.subscriber_emails) > 0 ? var.budget_config.subscriber_emails : [var.account_email]
+}
+
 resource "aws_organizations_account" "account" {
   name              = var.account_name
   email             = var.account_email
@@ -18,4 +23,34 @@ resource "aws_ssoadmin_account_assignment" "account_assignments" {
 
   target_id   = aws_organizations_account.account.id
   target_type = "AWS_ACCOUNT"
+}
+
+resource "aws_budgets_budget" "monthly_budget" {
+  name         = "overall-account-monthly-budget"
+  budget_type  = "COST"
+  limit_amount = var.budget_config.limit_amount
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+
+  dynamic "notification" {
+    for_each = var.budget_config.actual_thresholds_for_notification
+    content {
+      comparison_operator        = "GREATER_THAN"
+      threshold                  = notification.value
+      threshold_type             = "PERCENTAGE"
+      notification_type          = "ACTUAL"
+      subscriber_email_addresses = local.notification_emails
+    }
+  }
+
+  dynamic "notification" {
+    for_each = var.budget_config.forecast_thresholds_for_notification
+    content {
+      comparison_operator        = "GREATER_THAN"
+      threshold                  = notification.value
+      threshold_type             = "PERCENTAGE"
+      notification_type          = "ACTUAL"
+      subscriber_email_addresses = local.notification_emails
+    }
+  }
 }
