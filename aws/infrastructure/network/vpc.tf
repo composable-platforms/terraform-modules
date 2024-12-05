6219fd247c2159c80c302b866aeff441d03f01e9
Luke Diliberto <lukediliberto@gmail.com>
2024-12-05 09:34:42 -0800

    remove vpc endpoints

--- aws/infrastructure/network/vpc.tf
+++ aws/infrastructure/network/vpc.tf
@@ -149,49 +149,49 @@ resource "aws_route_table_association" "data" {
 }
 
 # Set up a VPC endpoint for S3
-resource "aws_vpc_endpoint" "s3" {
-  vpc_id            = aws_vpc.vpc.id
-  service_name      = "com.amazonaws.${var.aws_region}.s3"
-  vpc_endpoint_type = "Gateway"
+# resource "aws_vpc_endpoint" "s3" {
+#   vpc_id            = aws_vpc.vpc.id
+#   service_name      = "com.amazonaws.${var.aws_region}.s3"
+#   vpc_endpoint_type = "Gateway"
 
-  route_table_ids = aws_route_table.private.*.id
+#   route_table_ids = aws_route_table.private.*.id
 
-  tags = {
-    Name = "${var.stage}-vpc-endpoint-s3"
-  }
-}
+#   tags = {
+#     Name = "${var.stage}-vpc-endpoint-s3"
+#   }
+# }
 
-# Set up VPC endpoints for ECR (x2)
-resource "aws_vpc_endpoint" "ecr_api" {
-  vpc_id              = aws_vpc.vpc.id
-  service_name        = "com.amazonaws.${var.aws_region}.ecr.api"
-  vpc_endpoint_type   = "Interface"
-  private_dns_enabled = true
+# # Set up VPC endpoints for ECR (x2)
+# resource "aws_vpc_endpoint" "ecr_api" {
+#   vpc_id              = aws_vpc.vpc.id
+#   service_name        = "com.amazonaws.${var.aws_region}.ecr.api"
+#   vpc_endpoint_type   = "Interface"
+#   private_dns_enabled = true
 
-  security_group_ids = [aws_security_group.vpc_endpoints.id]
-  subnet_ids         = aws_subnet.private.*.id
+#   security_group_ids = [aws_security_group.vpc_endpoints.id]
+#   subnet_ids         = aws_subnet.private.*.id
 
-}
+# }
 
-resource "aws_vpc_endpoint" "ecr_dkr" {
-  vpc_id              = aws_vpc.vpc.id
-  service_name        = "com.amazonaws.${var.aws_region}.ecr.dkr"
-  vpc_endpoint_type   = "Interface"
-  private_dns_enabled = true
+# resource "aws_vpc_endpoint" "ecr_dkr" {
+#   vpc_id              = aws_vpc.vpc.id
+#   service_name        = "com.amazonaws.${var.aws_region}.ecr.dkr"
+#   vpc_endpoint_type   = "Interface"
+#   private_dns_enabled = true
 
-  security_group_ids = [aws_security_group.vpc_endpoints.id]
-  subnet_ids         = aws_subnet.private.*.id
+#   security_group_ids = [aws_security_group.vpc_endpoints.id]
+#   subnet_ids         = aws_subnet.private.*.id
 
-}
+# }
 
-# Set up VPC endpoint for secrets manager
-resource "aws_vpc_endpoint" "secretsmanager" {
-  vpc_id              = aws_vpc.vpc.id
-  service_name        = "com.amazonaws.${var.aws_region}.secretsmanager"
-  vpc_endpoint_type   = "Interface"
-  private_dns_enabled = true
+# # Set up VPC endpoint for secrets manager
+# resource "aws_vpc_endpoint" "secretsmanager" {
+#   vpc_id              = aws_vpc.vpc.id
+#   service_name        = "com.amazonaws.${var.aws_region}.secretsmanager"
+#   vpc_endpoint_type   = "Interface"
+#   private_dns_enabled = true
 
-  security_group_ids = [aws_security_group.vpc_endpoints.id]
-  subnet_ids         = aws_subnet.private.*.id
+#   security_group_ids = [aws_security_group.vpc_endpoints.id]
+#   subnet_ids         = aws_subnet.private.*.id
 
-}
+# }
